101676ececb83c2cddaf5e428e7affd54013be61
wryty <ikotytes@gmail.com>
2023-04-11 11:52:52 +0300

    1st

---
+++ client/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+namespace client
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        TelegramBotClient bot;
+
+        static ObservableCollection<TGUser> Users;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Users = new ObservableCollection<TGUser>();
+
+            usersList.ItemsSource = Users;
+
+            string token = "6286955732:AAEUOG2_l5LLqui1WyQSzxIdJte1kcjYnKk";
+            bot = new TelegramBotClient(token);
+
+
+            bot.StartReceiving(
+            async (bot, update, cts) =>
+            {
+                if (update.Message is Message message)
+                {
+                    string msg = $"{DateTime.Now}: {update.Message.Chat.FirstName} {update.Message.Chat.Id} {update.Message.Text}";
+
+
+                    //File.AppendAllText("data.log", $"{msg}\n");
+
+                    //Debug.WriteLine(msg);
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        var person = new TGUser(update.Message.Chat.FirstName, update.Message.Chat.Id);
+
+                        if (!Users.Contains(person)) Users.Add(person);
+
+                        Users[Users.IndexOf(person)].AddMessage($"{person.Nick}: {update.Message.Text}");
+                    });
+                }
+            }, Error);
+            btnSendMsg.Click += async delegate { await SendMsg(); };
+
+        }
+
+
+
+
+        public async Task SendMsg()
+        {
+            var concreteUser = Users[Users.IndexOf(usersList.SelectedItem as TGUser)];
+            string responseMsg = $"Support: {txtBxSendMsg.Text}";
+            concreteUser.Messages.Add(responseMsg);
+
+            await bot.SendTextMessageAsync(concreteUser.Id, txtBxSendMsg.Text);
+            //string logText = $"{DateTime.Now}: >> {concreteUser.Id} {concreteUser.Nick} {responseMsg}\n";
+            //File.AppendAllText("data.log", logText);
+
+            txtBxSendMsg.Text = String.Empty;
+        }
+        private static async Task Error(ITelegramBotClient bot, Exception e, CancellationToken cts)
+        {
+
+        }
+    }
+}
---
+++ client/TGUser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+namespace client
+{
+    internal class TGUser : INotifyPropertyChanged, IEquatable<TGUser>
+    {
+        public TGUser(string Nickname, long chatId)
+        {
+            this.nick = Nickname;
+            this.id = chatId;
+            Messages = new ObservableCollection<string>();
+        }
+
+        private string nick;
+        public string Nick
+        {
+            get { return this.nick; }
+            set
+            {
+                this.nick = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.Nick)));
+            }
+        }
+        private long id;
+        public long Id
+        {
+            get { return this.id; }
+            set
+            {
+                this.id = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.Id)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public bool Equals(TGUser other) => other.Id == this.id;
+
+
+        public ObservableCollection<string> Messages { get; set; }
+        public async void AddMessage(string text) => Messages.Add(text);
+    }
+}
